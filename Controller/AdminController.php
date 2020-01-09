9e4cc0b5d50a69b626fc9661c7fa4c68c4aaea68
Florian BERNARD <Cav0n@hotmail.fr>
2020-01-09 11:40:08 +0100

    Added documentation to AdminController

--- Controller/AdminController.php
+++ Controller/AdminController.php
@@ -17,26 +17,30 @@ class AdminController extends BaseAdminController
 {
     public function addSubjectAction()
     {
+        // Check if current user is Admin
         $authFail = $this->checkAuth(AdminResources::MODULE, ContactOptionBuilder::DOMAIN_NAME, AccessManager::CREATE);
         if ($authFail !== null) {
             return $authFail;
         }
 
-        $form = $this->createForm('contactoptionbuilder.subject.form');
+        $form = $this->createForm('contactoptionbuilder.subject.form'); // Create contact form
 
         try {
-            $this->validateForm($form);
+            $this->validateForm($form); // Validation of the form constraints
 
             /** @var COBService $cobService */
-            $cobService = $this->getContainer()->get('contactoptionbuilder.service');
+            $cobService = $this->getContainer()->get('contactoptionbuilder.service'); // Get COB service
 
+            // Get COB parameters (has order / need user connected)
             $orderListOption = $form->getForm()->get('cob_order_option')->getData();
             $userThelia = $form->getForm()->get('cob_user_thelia')->getData();
 
+            // Check if order list option is set but doesn't need user to be connected => impossible
             if (true === $orderListOption && false === $userThelia){
                 throw new FormValidationException(Translator::getInstance()->trans("Can't show order list only for user thelia"));
             }
 
+            // Get data from Form
             $dataConfiguration = [
                 'subject' => $form->getForm()->get('cob_subject')->getData(),
                 'message' => $form->getForm()->get('cob_message')->getData(),
@@ -46,6 +50,7 @@ class AdminController extends BaseAdminController
                 'email_to' => $form->getForm()->get('email_to')->getData(),
             ];
 
+            //Adding Form Option
             $cobService->addContactFormOption($dataConfiguration);
 
         } catch (FormValidationException $error_message) {
@@ -63,6 +68,7 @@ class AdminController extends BaseAdminController
             );
         }
 
+        // Redirect to module page
         return RedirectResponse::create(
             URL::getInstance()->absoluteUrl('/admin/module/' . ContactOptionBuilder::MODULE_CODE)
         );
@@ -70,16 +76,19 @@ class AdminController extends BaseAdminController
 
     public function delSubjectAction($idSubject)
     {
+        // Check if current user is Admin
         $authFail = $this->checkAuth(AdminResources::MODULE, ContactOptionBuilder::DOMAIN_NAME, AccessManager::CREATE);
         if ($authFail !== null) {
             return $authFail;
         }
 
+
         /** @var COBService $cobService */
-        $cobService = $this->getContainer()->get('contactoptionbuilder.service');
+        $cobService = $this->getContainer()->get('contactoptionbuilder.service'); // Get COB service
 
-        $cobService->delContactFormOption($idSubject);
+        $cobService->delContactFormOption($idSubject); // Delete option
 
+        // Redirect to module page
         return RedirectResponse::create(
             URL::getInstance()->absoluteUrl('/admin/module/' . ContactOptionBuilder::MODULE_CODE)
         );
